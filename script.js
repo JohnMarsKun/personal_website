a9378c968c65433960caef57ce1ecc8ac7a111b8
John Maris <88288021+JohnMarsKun@users.noreply.github.com>
2024-05-09 18:38:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -114,7 +114,7 @@ const homeContent = `
                         </li>
 
                         <li>
-                        <a class="course" href="src/proj/Time_Series_Assignment01-Ext.pdf" target="_blank"> Evaluating CO2 emission trends; forecasting solar irradiance in Hawaii using additive models with seasonal indicators to harmonic ones, using Quantile regression and analyzing Traffic Volume and Occupancies in Athens using Distributed Lag Models <i class="fas fa-link"> </i> </a>
+                        <a class="course" href="src/proj/Time_Series_Assignment01.pdf" target="_blank"> Evaluating CO2 emission trends; forecasting solar irradiance in Hawaii using additive models with seasonal indicators to harmonic ones, using Quantile regression and analyzing Traffic Volume and Occupancies in Athens using Distributed Lag Models <i class="fas fa-link"> </i> </a>
                         </li>
 
                         <li>
